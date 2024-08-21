4c1266e5ddbd27620827083a3cf65fafe918011e
9y06 <oygnijoes0209@gmail.com>
2024-08-21 19:38:30 +0900

    Initial Commit

---
+++ modurang/modurangApp.swift
@@ -0,0 +1,17 @@
+//
+//  modurangApp.swift
+//  modurang
+//
+//  Created by dgsw21 on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct modurangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
